b064eacef471c829bee5bd50bb643c9c0062028d
ajiu9 <615944323@qq.com>
2024-10-08 19:02:58 +0800

    feat: test

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -1,6 +1,8 @@
 import { afterAll, beforeAll, it } from 'vitest'
 import fs from 'fs-extra'
 import { join, resolve } from 'node:path'
+import { execa } from 'execa'
+
 
 beforeAll(async () => {
   await fs.rm('_fixtures', { recursive: true, force: true })
@@ -25,6 +27,25 @@ function runWithConfig(name: string, configs, ...items) {
       filter: (src) => {
         return !src.includes('node_modules')
       },
+    })
+    
+    await fs.writeFile(join(target, 'eslint.config.js'), `
+    // @eslint-disable
+    import ajiu9 from '@ajiu9/eslint-config'
+    
+    export default ajiu9(
+      ${JSON.stringify(configs)},
+      ...${JSON.stringify(items) ?? []},
+    )
+    console.log(ajiu9(
+      ${JSON.stringify(configs)},
+      ...${JSON.stringify(items) ?? []},
+    ))
+      `)
+
+    await execa('npx', ['eslint', '.', '--fix'], {
+      cwd: target,
+      stdio: 'pipe',
     })
 
   }, 30_000)
